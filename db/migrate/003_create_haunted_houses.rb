5148b40506bc066137d1b2cbf64aea6bef0bf2f3
Juan Carlo Banayo <johncban@gmail.com>
2019-10-18 02:00:30 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,10 @@
 # Create your haunted_houses migration here
+class CreateCostumes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costumes do |t|
+    
+
+      t.timestamps null: false
+    end
+  end
+end
