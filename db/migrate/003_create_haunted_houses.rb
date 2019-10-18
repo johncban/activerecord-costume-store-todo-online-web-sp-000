8270bcc769c88e78135cf8e8b9967c04dcee95b1
Juan Carlo Banayo <johncban@gmail.com>
2019-10-18 02:10:17 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,7 +2,14 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
   def change
     create_table :haunted_houses do |t|
-      
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :familyfriendly_or_not
+      t.datetime :open_date
+      t.datetime :close_date
+      t.text :description
 
       t.timestamps null: false
     end
