d7b029ebe138ba6902bf89a157b064cd5442c7a7
Juan Carlo Banayo <johncban@gmail.com>
2019-10-18 02:39:31 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,9 +6,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.string :location
       t.string :theme
       t.float :price
-      t.boolean :familyfriendly_or_not
-      t.datetime :open_date
-      t.datetime :close_date
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
       t.text :description
 
       t.timestamps null: false
